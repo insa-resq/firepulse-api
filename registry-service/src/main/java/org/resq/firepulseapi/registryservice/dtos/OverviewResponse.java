3eb1a0db312cce73a65a81583b54aa96546d88a5
Yamri002 <Yassine.amri002@gmail.com>
2025-12-28 14:07:46 +0100

    first version

---
+++ registry-service/src/main/java/org/resq/firepulseapi/registryservice/dtos/OverviewResponse.java
@@ -0,0 +1,28 @@
+package org.resq.firepulseapi.registryservice.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class OverviewResponse {
+    private Instant timestamp;
+    private int totalStations;
+    private int totalFirefighters;
+    private int totalVehicles;
+    private List<StationSummary> stations;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class StationSummary {
+        private String id;
+        private String name;
+        private int firefighterCount;
+        private int vehicleCount;
+    }
+}
